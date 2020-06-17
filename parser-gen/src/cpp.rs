6cdb89c6622a19d955407303383479f268436dee
MashPlant <740678788@qq.com>
2020-06-17 18:20:36 +0800

    cpp codegen minor update

--- parser-gen/src/cpp.rs
+++ parser-gen/src/cpp.rs
@@ -50,11 +50,11 @@ impl<F> Config<'_, F> {
           for i in 0..g.terms.len() as u32 {
             match act.get(&i) {
               Some(act) if !act.is_empty() => match act[0] {
-                Act::Acc => { let _ = write!(s, "Act{{.kind = Act::Acc, .val = 0}}, "); }
-                Act::Shift(x) => { let _ = write!(s, "Act{{.kind = Act::Shift, .val = {}}}, ", x); }
-                Act::Reduce(x) => { let _ = write!(s, "Act{{.kind = Act::Reduce, .val = {}}}, ", x); }
+                Act::Acc => { let _ = write!(s, "Act{{Act::Acc, 0}}, "); }
+                Act::Shift(x) => { let _ = write!(s, "Act{{Act::Shift, {}}}, ", x); }
+                Act::Reduce(x) => { let _ = write!(s, "Act{{Act::Reduce, {}}}, ", x); }
               }
-              _ => { let _ = write!(s, "Act{{.kind = Act::Err, .val = 0}}, "); }
+              _ => { let _ = write!(s, "Act{{Act::Err, 0}}, "); }
             }
           }
           let _ = write!(s, "}}, ");
@@ -71,16 +71,13 @@ impl<F> Config<'_, F> {
               Some(args) => args[j].0.as_deref().unwrap_or("_").to_owned(),
               None => format!("_{}", j + 1),
             };
-            if let Some(x) = g.as_nt(x) {
-              let _ = writeln!(s, "auto {}(std::move(*std::get_if<{}>(&value_stk.back()))); value_stk.pop_back();", name, g.nt[x].ty);
-            } else {
-              let _ = writeln!(s, "auto {}(std::move(*std::get_if<Token>(&value_stk.back()))); value_stk.pop_back();", name);
-            }
+            let ty = if let Some(x) = g.as_nt(x) {g.nt[x].ty} else {"Token"};
+            let _ = writeln!(s, "[[maybe_unused]] auto {}(std::move(*std::get_if<{}>(&value_stk.back()))); value_stk.pop_back();", name, ty);
           }
           if i == g.prod.len() - 1 {
-            let _ = writeln!(s, "value = _1;\nbreak;\n}}");
+            let _ = writeln!(s, "__ = _1;\nbreak;\n}}");
           } else {
-            let _ = writeln!(s, "{}\nbreak;\n}}", prod.act.replace("__", "value"));
+            let _ = writeln!(s, "{}\nbreak;\n}}", prod.act);
           }
         }
         s
